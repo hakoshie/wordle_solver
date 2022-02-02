ed0580ebd9cebef4d2ca0c6273bc7778f03ba14d
hakoshie <hakoshie@gmail.com>
2022-02-03 03:35:29 +0900

    edited the variables' names

--- wordle.py
+++ wordle.py
@@ -3,58 +3,57 @@ import nltk
 ts="abcdefghijklmnopqrstuvwxyz"
 print("input the characters not used in the word >>>",end=" ")
 sout=input()
-s=""
+cand_chars=""
 for i in range(len(ts)):
     if(ts[i] not in sout):
-        s+=ts[i]
-print("candidates",s)
-s=s.lower()
-n=len(s)
+        cand_chars+=ts[i]
+print("candidates",cand_chars)
+cand_chars=cand_chars.lower()
+n=len(cand_chars)
 print("input the known characters: apple -> a**le >>>",end=" ")
 known_str=input()
 known=list(known_str)
 print("input the hint characters >>>",end= " ")
-restrict=input()
-restrict=list(restrict)
-# for i in range(len(restrict)):
-#     resnums.append(ord(restrict[i])-ord("a"))
+hints=input()
+hints=list(hints)
+cand=set()
 def check(t):
     if len(nltk.corpus.wordnet.synsets(t))!=0:
         cand.add(t)
         return 1
     else:
         return 0
-cand=set()
+
 for i in range(n):
     for j in range(n):
         for k in range(n):
             for l in range(n):
-                t=""
+                tmp=""
                 idx=0
-                nums=[i,j,k,l]
-                used=[]
                 cnt=0
-                while len(t)<5:
+                indices=[i,j,k,l]
+                used=[]
+                while len(tmp)<5:
                     if(known[idx]!="*"):
-                        t+=known[idx]
+                        tmp+=known[idx]
                         cnt+=1
                     else:
-                        t+=s[nums[idx-cnt]]
-                        used.append(s[nums[idx-cnt]])
+                       tmp+=cand_chars[indices[idx-cnt]]
+                       used.append(cand_chars[indices[idx-cnt]])
                     idx+=1
                 
-                if(len(restrict)>0):
+                if(len(hints)>0):
                     ok=True
-                    for m in restrict:
+                    for m in hints:
                         if(m not in used):
                             ok=False
                             break
                     if(ok):
-                        check(t) 
+                        check(tmp) 
                         continue
                     else:
                         continue
-                check(t)
+                check(tmp)
 print("cands = ",cand)
 if(len(cand)>2):
     while(1):
